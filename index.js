2d5540f78b3a768bd5cfa29c1f8855ab33f13ead
hanseulhee <3021062@gmail.com>
2024-07-31 17:20:45 +0900

    [#1] feat: 안드로이드 font size default 설정

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import App from '@/screens/App.tsx';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import {name as appName} from './app.json';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.autoCorrect = false;
+TextInput.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
