78e2fc0b81f7e8a2352adaf0df93806d3e838842
minyoung <alsdyd1021@naver.com>
2024-08-04 03:04:50 +0900

    [#8] feat: tanstack query 초기 세팅

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,12 +1,17 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {SafeAreaView, Text} from 'react-native'
 import {RecoilRoot} from 'recoil'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <RecoilRoot>
-      <SafeAreaView>
-        <Text>baseball</Text>
-      </SafeAreaView>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaView>
+          <Text>baseball</Text>
+        </SafeAreaView>
+      </QueryClientProvider>
     </RecoilRoot>
   )
 }
