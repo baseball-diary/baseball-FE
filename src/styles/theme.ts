21ec0178ed0bb786ca5d86b8262db167a444913d
한슬희 <3021062@gmail.com>
2024-10-15 16:53:52 +0900

    [#12] style: theme 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    white: '#ffffff',
+    black: '#000000',
+    gray100: '#F6F6F6', // user background
+  },
+  fontWeight: {
+    light: 300,
+    normal: 500,
+    semiBold: 600,
+    bold: 700,
+  },
+}
+
+export type Theme = typeof theme
