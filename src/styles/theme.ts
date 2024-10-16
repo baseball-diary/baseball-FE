9d813827d0df38d0f50c43e67cbe3252e1146c19
한슬희 <3021062@gmail.com>
2024-10-16 09:34:32 +0900

    [#12] style: theme color 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,9 @@ export const theme = {
     white: '#ffffff',
     black: '#000000',
     gray100: '#F6F6F6', // user background
+    gray500: '#8B8B8B', // text
+    gray700: '#2C2C2C', // button
+    skyblue: '#0291D3',
   },
   fontWeight: {
     light: 300,
