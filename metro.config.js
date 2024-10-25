40c9924166bac6cd2dcdc96b9f6ebe2d92b2edb4
한슬희 <3021062@gmail.com>
2024-10-25 17:24:07 +0900

    [#15] fix: unable missing-asset-registry-path 해결

--- metro.config.js
+++ metro.config.js
@@ -7,17 +7,26 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config')
  * @type {import('metro-config').MetroConfig}
  */
 
-module.exports = (async () => {
-  const {
-    resolver: {sourceExts, assetExts},
-  } = await getDefaultConfig()
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  }
-})()
+const defaultConfig = getDefaultConfig(__dirname)
+
+const {
+  resolver: {sourceExts, assetExts},
+} = getDefaultConfig(__dirname)
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+}
+
+module.exports = mergeConfig(defaultConfig, config)
