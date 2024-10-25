7d67f663e0a3ab1373d9d4e2718b944a9113be3b
한슬희 <3021062@gmail.com>
2024-10-25 17:52:32 +0900

    [#15] feat: social icon svg 세팅

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export {default as GoogleLogin} from '@/assets/icons/social/googleLogin.svg'
+export {default as KakaoLogin} from '@/assets/icons/social/kakaoLogin.svg'
+export {default as NaverLogin} from '@/assets/icons/social/naverLogin.svg'
