1ceb44abb56756776886b79ff68545320a3d5330
한슬희 <3021062@gmail.com>
2024-10-28 11:44:46 +0900

    [#15] env: 직관일기 logo 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
+export {default as TextLogo} from '@/assets/icons/logo/textLogo.svg'
 export {default as GoogleLogin} from '@/assets/icons/social/googleLogin.svg'
 export {default as KakaoLogin} from '@/assets/icons/social/kakaoLogin.svg'
 export {default as NaverLogin} from '@/assets/icons/social/naverLogin.svg'
