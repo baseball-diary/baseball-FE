fd8f1a6a95f0e45f52c95b8337ef15f4037fb7e9
한슬희 <3021062@gmail.com>
2024-10-28 11:31:31 +0900

    [#15] fix: font otf 확장자 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,16 +2,18 @@ export const theme = {
   color: {
     white: '#ffffff',
     black: '#000000',
-    gray100: '#F6F6F6', // user background
+    gray100: '#F0F0F0', // user background
     gray500: '#8B8B8B', // text
     gray700: '#2C2C2C', // button
     skyblue: '#0291D3',
   },
   fontWeight: {
     light: 300,
-    normal: 500,
+    medium: 400,
+    regular: 500, // default
     semiBold: 600,
     bold: 700,
+    extraBold: 800,
   },
 }
 
